09318feba8155c286470f66b229517deff377639
laneyching <lching@wustl.edu>
2020-12-06 18:13:56 -0600

    questions in

--- spotify/authorization_code/public/home.js
+++ spotify/authorization_code/public/home.js
@@ -39,237 +39,245 @@ let questions = [
             }
         ]  
     },
-    //     
-    // {
-    //     q: "Which version of 'Baby' is your favorite?",
-    //     answers: 4,
-    //     a1 = {
-    //         name: "Baby by Justin Bieber",
-    //         image: "baby.jpg",
-    //         genre: "pop"
-    //     },
-    //     a2 = {
-    //         name: "Babysitter by Dababy",
-    //         image: "baby-sitter.jpg",
-    //         genre: "hiphop"
-    //     },
-    //     a3 = {
-    //         name: "Babydoll by Dominic Fike",
-    //         image: "babydoll.jpg",
-    //         genre: "rock"
-    //     },
-    //     a4 = {
-    //         name: "Baby its Cold Outside by Frank Sinatra",
-    //         image: "baby-its-cold-outside.jpg",
-    //         genre: "jazz"
-    //     },
-    // },
-    // {
-    //     q: "Old Taylor Swift or New Taylor Swift?",
-    //     answers: 4,
-    //     a1 = {
-    //         name: "Old Taylor",
-    //         image: "old-taylor.jpg",
-    //         genre: "country"
-    //     },
-    //     a2 = {
-    //         name: "New Taylor",
-    //         image: "new-taylor.jpg",
-    //         genre: "pop"
-    //     }
-    // },
-    // {
-    //     q: "Pick a geographic location.",
-    //     answers: 4,
-    //     a1 = {
-    //         name: "Californiacation by The Red Hot Chilli Peppers",
-    //         image: "californiacation.jpg",
-    //         genre: "rock"
-    //     },
-    //     a2 = {
-    //         name: "Sweet Home Alabama by Lynyrd Skynyrd",
-    //         image: "sweet-home-alabama.jpg",
-    //         genre: "country"
-    //     },
-    //     a3 = {
-    //         name: "Empire State of Mind by Alecia Keys",
-    //         image: "empire-state-of-mind.png",
-    //         genre: "randb"
-    //     },
-    //     a4 = {
-    //         name: "Paris by the Chainsmokers",
-    //         image: "paris.png",
-    //         genre: "electronic"
-    //     },
-    // },
-    // {
-    //     q: "Stay?", 
-    //     answers: 4,
-    //     a1 = {
-    //         name: "Should I Stay or Should I Go by the Clash",
-    //         image: "should-i-stay-or-should-i-go.jpg",
-    //         genre: "rock"
-    //     },
-    //     a2 = {
-    //         name: "Stay by Rihanna",
-    //         image: "stay.png",
-    //         genre: "randb"
-    //     },
-    //     a3 = {
-    //         name: "Stay by Zedd & Alessia Cara",
-    //         image: "stay-zedd.jpg",
-    //         genre: "electronic"
-    //     },
-    //     a4 = {
-    //         name: "Lets Stay Together by Al Green",
-    //         image: "lets-stay-together.jpg",
-    //         genre: "jazz"
-    //     },
-    // },
-    // {
-    //     q: "Ya like jazz?", 
-    //     answers: 4,
-    //     a1 = {
-    //         name: "I love jazz!",
-    //         image: "i-love-jazz.jpg",
-    //         genre: "jazz"
-    //     },
-    //     a2 = {
-    //         name: "I hate jazz.",
-    //         image: "i-hate-jazz.jpg",
-    //         genre: "electronic"
-    //     },
-    //     a3 = {
-    //         name: "I love the Bee Movie",
-    //         image: "bee-movie-png",
-    //         genre: "pop"
-    //     },
-    //     a4 = {
-    //         name: "Oh that's from that movie my kids made me watch",
-    //         image: "ya-like-jazz.png",
-    //         genre: "classical"
-    //     },
-    // },
-    // {
-    //     q: "How do you like your USA?",
-    //     answers: 4,
-    //     a1 = {
-    //         name: "American Idiot by Greenday",
-    //         image: "american-idiot.jpg",
-    //         genre: "rock"
-    //     },
-    //     a2 = {
-    //         name: "This is America by Childish Gambino",
-    //         image: "this-is-america.jpg",
-    //         genre: "hiphop"
-    //     },
-    //     a3 = {
-    //         name: "Party in the USA Miley Cyrus?",
-    //         image: "party-in-the-usa.jpg",
-    //         genre: "pop"
-    //     },
-    //     a4 = {
-    //         name: "American Boy Estelle",
-    //         image: "american-boy.jpg",
-    //         genre: "randb"
-    //     },
-    // },
-
-    // {
-    //     q: "Bodies but like, only in a weird way if you make it weird.",
-    //     answers: 4,
-    //     a1 = {
-    //         name: "Body by Loud Luxury",
-    //         image: "body.png",
-    //         genre: "electronic"
-    //     },
-    //     a2 = {
-    //         name: "Touch my Body by Maria Carey",
-    //         image: "touch-my-body.jpg",
-    //         genre: "randb"
-    //     },
-    //     a3 = {
-    //         name: "Talking Body by Tove Lo",
-    //         image: "talking-body.jpg",
-    //         genre: "pop"
-    //     },
-    //     a4 = {
-    //         name: "Somebody to Love by Queen",
-    //         image: "somebody-to-love.jpg",
-    //         genre: "rock"
-    //     },
-    // },
-
-    // {
-    //     q: "Pick an accessory.",
-    //     answers: 4,
-    //     a1 = {
-    //         name: "Cowboy Boots",
-    //         image: "cowboy.boots.jpg",
-    //         genre: "country"
-    //     },
-    //     a2 = {
-    //         name: "Rave Kandi Bracelet",
-    //         image: "rave-kandi-bracelet.jpg",
-    //         genre: "electronic"
-    //     },
-    //     a3 = {
-    //         name: "Healing Crystals",
-    //         image: "healing-crystals.jpg",
-    //         genre: "randb"
-    //     },
-    //     a4 = {
-    //         name: "Fedora",
-    //         image: "fedora.jpg",
-    //         genre: "jazz"
-    //     },
-    // },
-    // {
-    //     q: "Pick a vibey space.",
-    //     answers: 4,
-    //     a1 = {
-    //         name: "Music Festival",
-    //         image: "festival.jpg",
-    //         genre: "pop"
-    //     },
-    //     a2 = {
-    //         name: "Underground Bar",
-    //         image: "underground-bar.jpg",
-    //         genre: "rock"
-    //     },
-    //     a3 = {
-    //         name: "County Fair",
-    //         image: "",
-    //         genre: "country"
-    //     },
-    //     a4 = {
-    //         name: "Coffee Shop",
-    //         image: "coffee-stop.jpg",
-    //         genre: "jazz"
-    //     },
-    //     a5 = {
-    //         name: "Symphony Hall",
-    //         image: "symphony-hall.jpg",
-    //         genre: "classical"
-    //     },
-    //     a6 = {
-    //         name: "Rave",
-    //         image: "rave.jpg",
-    //         genre: "electronic"
-    //     },
-    //     a7 = {
-    //         name: "Driving with the Windows Down",
-    //         image: "car-radio.jpg",
-    //         genre: "hiphop"
-    //     },
-    //     a8 = {
-    //         name: "Bedroom",
-    //         image: "",
-    //         genre: "randb"
-    //     },
-    // },
-    
-]
+    {
+        q: "Which version of 'Baby' is your favorite?",
+        answers: [
+            {
+                name: "Baby by Justin Bieber",
+                image: "baby.jpg",
+                genre: "pop"
+            },
+            {
+                name: "Babysitter by Dababy",
+                image: "baby-sitter.jpg",
+                genre: "hiphop"
+            },
+            {
+                name: "Babydoll by Dominic Fike",
+                image: "babydoll.jpg",
+                genre: "rock"
+            },
+            {
+                name: "Baby its Cold Outside by Frank Sinatra",
+                image: "baby-its-cold-outside.jpg",
+                genre: "jazz"
+            }
+        ]
+    },
+    {
+        q: "Old Taylor Swift or New Taylor Swift?",
+        answers: [
+            {
+                name: "Old Taylor",
+                image: "old-taylor.jpg",
+                genre: "country"
+            },
+            {
+                name: "New Taylor",
+                image: "new-taylor.jpg",
+                genre: "pop"
+            }
+        ]
+    },
+    {
+        q: "Pick a geographic location.",
+        answers: [
+            {
+                name: "Californiacation by The Red Hot Chilli Peppers",
+                image: "californiacation.jpg",
+                genre: "rock"
+            },
+            {
+                name: "Sweet Home Alabama by Lynyrd Skynyrd",
+                image: "sweet-home-alabama.jpg",
+                genre: "country"
+            },
+            {
+                name: "Empire State of Mind by Alecia Keys",
+                image: "empire-state-of-mind.png",
+                genre: "randb"
+            },
+            {
+                name: "Paris by the Chainsmokers",
+                image: "paris.png",
+                genre: "electronic"
+            }
+        ]
+        
+    },
+    {
+        q: "Stay?", 
+        answers: [
+            {
+                name: "Should I Stay or Should I Go by the Clash",
+                image: "should-i-stay-or-should-i-go.jpg",
+                genre: "rock"
+            },
+            {
+                name: "Stay by Rihanna",
+                image: "stay.png",
+                genre: "randb"
+            },
+            {
+                name: "Stay by Zedd & Alessia Cara",
+                image: "stay-zedd.jpg",
+                genre: "electronic"
+            },
+            {
+                name: "Lets Stay Together by Al Green",
+                image: "lets-stay-together.jpg",
+                genre: "jazz"
+            }
+        ]
+        
+    },
+    {
+        q: "Ya like jazz?", 
+        answers: [
+            {
+                name: "I love jazz!",
+                image: "i-love-jazz.jpg",
+                genre: "jazz"
+            },
+            {
+                name: "I hate jazz.",
+                image: "i-hate-jazz.jpg",
+                genre: "electronic"
+            },
+            {
+                name: "I love the Bee Movie",
+                image: "bee-movie-png",
+                genre: "pop"
+            },
+            {
+                name: "Oh that's from that movie my kids made me watch",
+                image: "ya-like-jazz.png",
+                genre: "classical"
+            }
+        ]
+        
+    },
+    {
+        q: "How do you like your USA?",
+        answers: [
+            {
+                name: "American Idiot by Greenday",
+                image: "american-idiot.jpg",
+                genre: "rock"
+            },
+            {
+                name: "This is America by Childish Gambino",
+                image: "this-is-america.jpg",
+                genre: "hiphop"
+            },
+            {
+                name: "Party in the USA Miley Cyrus?",
+                image: "party-in-the-usa.jpg",
+                genre: "pop"
+            },
+            {
+                name: "American Boy Estelle",
+                image: "american-boy.jpg",
+                genre: "randb"
+            }
+        ]
+    },
+    {
+        q: "Bodies but like, only in a weird way if you make it weird.",
+        answers: [
+            {
+                name: "Body by Loud Luxury",
+                image: "body.png",
+                genre: "electronic"
+            },
+            {
+                name: "Touch my Body by Maria Carey",
+                image: "touch-my-body.jpg",
+                genre: "randb"
+            },
+            {
+                name: "Talking Body by Tove Lo",
+                image: "talking-body.jpg",
+                genre: "pop"
+            },
+            {
+                name: "Somebody to Love by Queen",
+                image: "somebody-to-love.jpg",
+                genre: "rock"
+            }
+        ]  
+    },
+    {
+        q: "Pick an accessory.",
+        answers: [
+            {
+                name: "Cowboy Boots",
+                image: "cowboy.boots.jpg",
+                genre: "country"
+            },
+            {
+                name: "Rave Kandi Bracelet",
+                image: "rave-kandi-bracelet.jpg",
+                genre: "electronic"
+            },
+            {
+                name: "Healing Crystals",
+                image: "healing-crystals.jpg",
+                genre: "randb"
+            },
+            {
+                name: "Fedora",
+                image: "fedora.jpg",
+                genre: "jazz"
+            }
+        ] 
+    },
+    {
+        q: "Pick a vibey space.",
+        answers: [
+            {
+                name: "Music Festival",
+                image: "festival.jpg",
+                genre: "pop"
+            },
+            {
+                name: "Underground Bar",
+                image: "underground-bar.jpg",
+                genre: "rock"
+            },
+            {
+                name: "County Fair",
+                image: "",
+                genre: "country"
+            },
+            {
+                name: "Coffee Shop",
+                image: "coffee-stop.jpg",
+                genre: "jazz"
+            },
+            {
+                name: "Symphony Hall",
+                image: "symphony-hall.jpg",
+                genre: "classical"
+            },
+            {
+                name: "Rave",
+                image: "rave.jpg",
+                genre: "electronic"
+            },
+            {
+                name: "Driving with the Windows Down",
+                image: "car-radio.jpg",
+                genre: "hiphop"
+            },
+            {
+                name: "Bedroom",
+                image: "",
+                genre: "randb"
+            }
+        ]
+    }
+]    
 
 
 answers = []
